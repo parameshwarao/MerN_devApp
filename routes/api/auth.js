6322499379a305378fe2f727c738a5ffca22a5c6
parameshwarao <parameshwarrao.s@gmail.com>
2020-04-23 18:45:48 +0530

    register signup login authentication apis

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,85 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
 
 // @route GET api/auth
 // @desc  Test route
 // @access Public
 
-router.get('/', (req, res) => res.send('auth Route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route POST api/auth
+// @desc  Authenticate user & get token
+// @access Public
+
+router.post(
+  '/',
+  [
+    check('email', 'Please include a Valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      //see if user exists
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      //Return jsonwebtoken
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtsSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 //connect database
 connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send(`API running`));
 
 // Define Routes
